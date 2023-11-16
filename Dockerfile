597e426a78fc5e2740afd23aa3e577c017f7bd5d
Joaquin Guerrini <joaquinguerrini1@gmail.com>
2023-11-16 16:50:33 -0300

    feat: dockerizado de app y ngrok

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM node:18-alpine3.15
+
+# Set working directory
+RUN mkdir -p /var/www/kai
+WORKDIR /var/www/kai
+
+# Copiar el directorio y su contenido
+COPY . ./var/www/kai
+COPY package.json tsconfig.json tsconfig.build.json serviceAccount.json /var/www/kai/
+
+# Instala el Nest CLI globalmente
+RUN npm install -g @nestjs/cli
+
+# Instalar las librerías necesarias
+RUN npm install --omit=dev
+
+# Buildear la APP
+RUN npm run build
+
+# Limpiar el caché
+RUN npm cache clean --force
+
+EXPOSE 5000
+
+#start for dev
+CMD [ "npm", "run", "start:dev" ]
+#start for prod
+# CMD [ "npm", "start"]
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 import * as admin from 'firebase-admin';
-import { exec } from 'child_process';
 import fs from 'fs';
+import path from 'path';
 
 export const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
@@ -29,56 +29,13 @@ export const bootstrap = async () => {
     }),
   );
 
-  await startNgrok();
-
-  const serviceAccount = JSON.parse(fs.readFileSync('serviceAccount.json', 'utf-8'));
+  const serviceAccount = JSON.parse(fs.readFileSync(path.join('serviceAccount.json'), 'utf-8'));
   admin.initializeApp({
     credential: admin.credential.cert({ ...serviceAccount }),
   });
 
   await app.listen(process.env.PORT, () => {
     console.log('Server running on port: ', process.env.PORT);
-    console.log('Tunnel online at ', process.env.NGROK_DOMAIN);
   });
 };
 bootstrap();
-
-const startNgrok = async () => {
-  return new Promise(async (resolve) => {
-    const isRunning = await ngrokCheckIsRunning();
-    console.log(isRunning ? 'El servicio ya está corriendo en ngrok' : 'Levantando el servicio en ngrok...');
-    if (!isRunning) {
-      console.log('Agregando el token de ngrok...');
-      await ngrokExecAddToken();
-      console.log('Iniciando el tunel...');
-      await ngrokExecStart();
-    }
-    resolve(true);
-  });
-};
-
-const ngrokCheckIsRunning = async () => {
-  const checkIsRunning = `ngrok api tunnels list --api-key=${process.env.NGROK_API_KEY}`;
-  return new Promise((resolve, reject) => {
-    exec(checkIsRunning, (error, stdout) => {
-      if (error) reject(error);
-      const res = JSON.parse(stdout.trim());
-      resolve(res?.tunnels?.some((tunnel) => String(tunnel.public_url).endsWith(process.env.NGROK_DOMAIN)));
-    });
-  });
-};
-
-const ngrokExecAddToken = async () => {
-  const addTokenCommand = `ngrok config add-authtoken ${process.env.NGROK_TOKEN}`;
-  return new Promise((resolve, reject) => {
-    exec(addTokenCommand, (error, stdout) => {
-      if (error) reject(error);
-      resolve(stdout.trim());
-    });
-  });
-};
-
-const ngrokExecStart = async () => {
-  const startTunnelCommand = `ngrok http --domain=${process.env.NGROK_DOMAIN} ${process.env.PORT}`;
-  exec(startTunnelCommand);
-};
