65642cdc9880a26ca97c678d889ceb1355196b4a
Joaquin Guerrini <joaquinguerrini1@gmail.com>
2023-12-04 10:22:00 -0300

    feat: request logs added

--- src/common/middlewares/Logger.middleware.ts
+++ src/common/middlewares/Logger.middleware.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request } from 'express';
 
 export function LoggerMiddleware(req: Request, res: Response, next: NextFunction) {
-  console.log(req);
   if (req.method.toUpperCase() === 'OPTIONS') return next();
   console.log(`${req.method.toUpperCase()}: ${req.baseUrl}`);
   console.log(`Body: ${JSON.stringify(req.body)}`);
